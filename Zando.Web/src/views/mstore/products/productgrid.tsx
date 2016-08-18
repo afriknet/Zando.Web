30925108c6d8b07d10bf2ca54b3878a36f9008c2
Seya Obey <seyaobey@gmail.com>
2016-08-18 13:15:28 +0300

    product grid, product box view

--- Zando.Web/src/views/mstore/products/productgrid.tsx
+++ Zando.Web/src/views/mstore/products/productgrid.tsx
@@ -1,4 +1,5 @@
-﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+﻿/// <reference path="productgrid_listview.tsx" />
+// A '.tsx' file enables JSX support in the TypeScript compiler, 
 // for more information see the following page on the TypeScript wiki:
 // https://github.com/Microsoft/TypeScript/wiki/JSX
 
@@ -8,13 +9,24 @@ import React = require('react');
 import ReactDOM = require('react-dom');
 import jx = require('../../../lib/jx');
 import base = require('../lib/basepage');
+import list = require('./productgrid_listview');
+
 
 
 
 export class ProductGridPage extends base.BasePage {
 
     get_pagecontent() {
-        return <div style={{ minHeight: 430 }}>Product grid page</div>;
+        var html =
+            <div className="row">
+
+                <GridFilters />
+
+                <list.ProductGridListView />
+
+            </div>
+
+        return html;
     }
 
 
@@ -29,3 +41,89 @@ export class ProductGridPage extends base.BasePage {
     }
 
 }
+
+
+
+class GridFilters extends jx.Views.ReactView{
+    render(){
+
+        var html =
+
+        <div className="col-sm-3">
+          <div className="filter-sidebar">
+            <div className="title"><span>Enabled Filters</span></div>
+            <ul>
+              <li>Categories: T-Shirts <a href="#" className="remove-filter"><i className="fa fa-remove" /></a></li>
+              <li>Availability: In Stock <a href="#" className="remove-filter"><i className="fa fa-remove" /></a></li>
+              <li>Brand: Brand Name 1 <a href="#" className="remove-filter"><i className="fa fa-remove" /></a></li>
+            </ul>
+          </div>
+          <div className="filter-sidebar">
+            <div className="title"><span>Categories</span></div>
+            <ul>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="DressCheckbox" defaultValue="checked" /><label htmlFor="DressCheckbox">T-Shirts (10)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="SweaterCheckbox" /><label htmlFor="SweaterCheckbox">Polo T-Shirts (11)</label></div>
+              </li>
+            </ul>
+          </div>
+          <div className="filter-sidebar">
+            <div className="title"><span>Availability</span></div>
+            <ul>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="InStockCheckbox" defaultValue="checked" /><label htmlFor="InStockCheckbox">In Stock (20)</label></div>
+              </li>
+            </ul>
+          </div>
+          <div className="filter-sidebar">
+            <div className="title"><span>Brand</span></div>
+            <ul>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="BrandName1Checkbox" defaultValue="checked" /><label htmlFor="BrandName1Checkbox">Brand Name 1 (11)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="BrandName2Checkbox" /><label htmlFor="BrandName2Checkbox">Brand Name 2 (12)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="BrandName3Checkbox" /><label htmlFor="BrandName3Checkbox">Brand Name 3 (13)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="BrandName4Checkbox" /><label htmlFor="BrandName4Checkbox">Brand Name 4 (14)</label></div>
+              </li>
+            </ul>
+          </div>
+          <div className="filter-sidebar">
+            <div className="title"><span>Price Range</span></div>
+            <div id="range-value">Range: <span id="min-price" /> - <span id="max-price" /></div>
+            <input type="hidden" name="min-price" defaultValue="" />
+            <input type="hidden" name="max-price" defaultValue="" />
+            <div className="price-range">
+              <div id="price" />
+            </div>
+          </div>
+          <div className="filter-sidebar">
+            <div className="title"><span>Size</span></div>
+            <ul>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="SCheckbox" /><label htmlFor="SCheckbox">S (11)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="MCheckbox" /><label htmlFor="MCheckbox">M (12)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="LCheckbox" /><label htmlFor="LCheckbox">L (13)</label></div>
+              </li>
+              <li>
+                <div className="checkbox"><input type="checkbox" id="XLCheckbox" /><label htmlFor="XLCheckbox">XL (14)</label></div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        return html;
+
+    }
+}
+
---
+++ Zando.Web/src/views/mstore/products/productgrid_listview.tsx
@@ -0,0 +1,221 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+
+
+import React = require('react');
+import ReactDOM = require('react-dom');
+import jx = require('../../../lib/jx');
+import gn = require('../../lib/redux/generic_workflow');
+
+
+
+export interface ProductGridListViewProps extends jx.Views.ReactProps{
+}
+interface ProductGridListViewState extends jx.Views.ReactState{
+    items: any[];
+}
+
+export class ProductGridListView extends jx.Views.ReactView{
+
+    props:ProductGridListViewProps;
+    state:ProductGridListViewState;
+    unfiltered_count:number;
+    
+
+    constructor(props:ProductGridListViewProps){
+
+        super(props);
+        
+        this.state.loading = true;
+
+    }
+
+    componentDidMount(){
+
+        if(this.state.loading){
+
+            utils.spin(this.root);
+
+            this.fetch_page(1).then((data)=>{
+
+                this.setState(_.extend(this.state, {
+                    items: data,
+                    loading: false
+                }));
+
+            }).finally(()=>{
+
+                utils.unspin(this.root);
+
+            });
+            
+        }
+    }
+
+    
+    query_count(): Q.Promise<number> {
+
+        var d = Q.defer<number>();
+
+        schema.call({
+            fn: 'get',
+            params: ['/products/:count', {
+                //active: true
+            }]
+        }).then(res => {
+            d.resolve(res.response as any);
+        }).fail(err => {
+
+        });
+
+        return d.promise;
+
+    }
+
+
+    fetch_page(activepage: number) {
+        
+        var d = Q.defer();
+
+        this.query_count().then(count => {
+
+            this.unfiltered_count = count;
+            
+            schema.call({
+                fn: 'get',
+                params: ['/products', {
+                    limit: 8,
+                    page: activepage
+                }]
+            }).then((res) => {
+                
+                d.resolve(res.response.results);
+
+            }).fail((err) => {
+                d.reject(err);
+            });
+            
+        });
+        
+        return d.promise;   
+    }
+
+
+    render() {
+
+        var that = this;
+
+        function resolve_content():any {
+
+            if (that.state.loading) {
+                return <div style={{ minHeight: 350 }}></div>
+            }
+
+            var products = _.map(that.state.items, item => {
+                                return <ProductBoxView />
+            })
+
+            return [products, <GridPagination />]
+        }
+        
+
+        var html =
+
+            <div className="col-sm-9" style={{ minHeight:350 }}>
+          <div className="title"><span>T-Shirts</span></div>
+          <div className="product-sorting-bar">
+            <div>Sort By
+              <select name="sortby" className="select2">
+                <option value="">Recomended</option>
+                <option value="">Low Price » High Price</option>
+                <option value="">High Price » High Price</option>
+              </select>
+            </div>
+            <div>Show
+              <select name="show" className="select2">
+                <option value="">8</option>
+                <option value="">12</option>
+                <option value="">16</option>
+              </select> per page
+            </div>
+          </div>
+
+          { resolve_content() }
+                
+        </div>
+
+
+        return html;
+    }    
+}
+
+
+class GridPagination extends jx.Views.ReactView {
+
+    render() {
+
+        var html =
+
+            <div className="col-xs-12 text-center">
+                <ul className="pagination">
+                    <li className="disabled"><a href="#">«</a></li>
+                    <li className="disabled"><a href="#">‹</a></li>
+                    <li className="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">›</a></li>
+                    <li><a href="#">»</a></li>
+                </ul>
+            </div>
+
+
+        return html;
+    }
+}
+
+
+
+class ProductBoxView extends jx.Views.ReactView{
+
+    render(){
+    
+        var html =
+        <div className="col-sm-4 col-md-3 box-product-outer">
+            <div className="box-product">
+              <div className="img-wrapper">
+                <a href="detail.html">
+                  <img alt="Product" src="/mstore/images/demo/polo1.jpg" />
+                </a>
+                <div className="tags">
+                  <span className="label-tags"><span className="label label-danger">Hot Item</span></span>
+                </div>
+                <div className="option">
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Add to Cart"><i className="ace-icon fa fa-shopping-cart" /></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Compare"><i className="ace-icon fa fa-align-left" /></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Wishlist"><i className="ace-icon fa fa-heart" /></a>
+                </div>
+              </div>
+              <h5><a href="detail.html">IncultGeo Print Polo T-Shirt</a></h5>
+              <div className="price">
+                <div>$16.59<span className="price-down">-10%</span></div>
+                <span className="price-old">$15.00</span>
+              </div>
+              <div className="rating">
+                <i className="ace-icon fa fa-star" />
+                <i className="ace-icon fa fa-star" />
+                <i className="ace-icon fa fa-star" />
+                <i className="ace-icon fa fa-star-half-o" />
+                <i className="ace-icon fa fa-star-o" />
+                <a href="#">(5 reviews)</a>
+              </div>
+            </div>
+          </div>
+        
+        
+        return html;    
+        
+    }    
+    
+} 
